2f9023af2cdbd84d4cb0748d5626ab99529084a1
Aidan Wolter <wolter.aidan@gmail.com>
2015-02-23 19:48:43 -0600

    add working csvs of the slp

--- syn.cpp
+++ syn.cpp
@@ -76,6 +76,7 @@ int main(int argc, char *argv[]) {
   int t_stack_cnt = 0;
   int s_stack_cnt = 1;
   struct token t = lex_next_token(lex, infile);
+  //printf("t: %d %s\n", t.t, t.l);
   while(true) {
 
     //error in lexer
@@ -87,6 +88,10 @@ int main(int argc, char *argv[]) {
     //newline
     else if(t.t == -2) {
       line++;
+
+      //get another token
+      t = lex_next_token(lex, infile);
+      //printf("t: %d %s\n", t.t, t.l);
     }
 
     //follow the table
@@ -133,6 +138,8 @@ int main(int argc, char *argv[]) {
         return 0;
       }
 
+      //printf("cmd: %s\n", cmd);
+
       //reduction
       if(cmd[0] == 'r') {
 
@@ -142,17 +149,105 @@ int main(int argc, char *argv[]) {
         struct token rep_token;//what to push onto the token stack
 
         //check each production
-        if(prod == 0) {
-          pop_size = 5; //print();
-          rep_token.t = 5;
+
+        //S;S
+        if(prod == 1) {
+          pop_size = 3;
+          rep_token.t = 13;
           rep_token.l[0] = 'S';
         }
-        else if(prod == 1) {
-          pop_size = 0; //epsilon
-          rep_token.t = 5;
-          strcpy(rep_token.l, "epsilon");
+
+        //id:=E
+        else if(prod == 2) {
+          pop_size = 3;
+          rep_token.t = 13;
+          rep_token.l[0] = 'S';
+        }
+
+        //print(L)
+        else if(prod == 3) {
+          pop_size = 4;
+          rep_token.t = 13;
+          rep_token.l[0] = 'S';
+        }
+
+        //epsilon
+        else if(prod == 4) {
+          pop_size = 0;
+          rep_token.t = 13;
+          rep_token.l[0] = 'S';
+        }
+
+        //id
+        else if(prod == 5) {
+          pop_size = 1;
+          rep_token.t = 14;
+          rep_token.l[0] = 'E';
+        }
+
+        //num
+        else if(prod == 6) {
+          pop_size = 1;
+          rep_token.t = 14;
+          rep_token.l[0] = 'E';
+        }
+
+        //EBE
+        else if(prod == 7) {
+          pop_size = 3;
+          rep_token.t = 14;
+          rep_token.l[0] = 'E';
+        }
+
+        //(S,E)
+        else if(prod == 8) {
+          pop_size = 5;
+          rep_token.t = 14;
+          rep_token.l[0] = 'E';
+        }
+
+        //E,L
+        else if(prod == 9) {
+          pop_size = 3;
+          rep_token.t = 15;
+          rep_token.l[0] = 'L';
+        }
+
+        //E
+        else if(prod == 10) {
+          pop_size = 1;
+          rep_token.t = 15;
+          rep_token.l[0] = 'L';
+        }
+
+        //+
+        else if(prod == 11) {
+          pop_size = 1;
+          rep_token.t = 16;
+          rep_token.l[0] = 'B';
+        }
+
+        //-
+        else if(prod == 12) {
+          pop_size = 1;
+          rep_token.t = 16;
+          rep_token.l[0] = 'B';
         }
         
+        //*
+        else if(prod == 13) {
+          pop_size = 1;
+          rep_token.t = 16;
+          rep_token.l[0] = 'B';
+        }
+
+        // /
+        else if(prod == 14) {
+          pop_size = 1;
+          rep_token.t = 16;
+          rep_token.l[0] = 'B';
+        }
+
         //pop off the size of the production
         s_stack_cnt -= pop_size;
         t_stack_cnt -= pop_size;
@@ -173,6 +268,10 @@ int main(int argc, char *argv[]) {
         int newstate = strtod(&cmd[1], &stopstr);
         t_stack[t_stack_cnt++] = t.t;
         s_stack[s_stack_cnt++] = newstate;
+
+        //get another token
+        t = lex_next_token(lex, infile);
+        //printf("t: %d %s\n", t.t, t.l);
       }
 
       //accept command
@@ -186,10 +285,20 @@ int main(int argc, char *argv[]) {
         printf("Error: Parse table contains unknown command (%s)\n", cmd);
         return 0;
       }
+
+
+      //printf("t_stack: ");
+      //for(int i = 0; i < t_stack_cnt; i++) {
+      //  printf("%d, ", t_stack[i]);
+      //}
+      //printf("\ns_stack: ");
+      //for(int i = 0; i < s_stack_cnt; i++) {
+      //  printf("%d, ", s_stack[i]);
+      //}
+      //printf("\n");
     }
 
-    //get another token
-    t = lex_next_token(lex, infile);
+    
   }
 
   return 0;
@@ -209,10 +318,10 @@ int parse_table_col_from_token(struct token t, char ***parse_table, int cols) {
 //parse a csv with a variable size
 char*** parse_csv(FILE *file, int *rows, int *cols) {
   char ***table;
-  char *line = (char*)malloc(1024*sizeof(char*)); //bigger?
+  char *line = (char*)malloc(10000*sizeof(char*)); //bigger?
   *rows = 1;
   *cols = 1;
-  while(fgets(line, 1024, file)) {
+  while(fgets(line, 10000, file)) {
 
     //count the number of rows and columns
     bool cols_state = false;
@@ -308,6 +417,13 @@ char*** parse_csv(FILE *file, int *rows, int *cols) {
       }
     }
   }
+
+  //for(int i = 0; i < *rows; i++) {
+  //  for(int j = 0; j < *cols; j++) {
+  //    printf("%s ", table[i][j]);
+  //  }
+  //  printf("\n");
+  //}
 
   return table;
 }
